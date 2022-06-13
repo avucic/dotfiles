f2bd8d3802c5120c1a9b47fa4dabe426489279e9
Aleksandar Vucic <vucinjo@gmail.com>
2022-06-13 06:56:47 +0200

    update diffview keybindings

--- nvim_user/.config/nvim/lua/user/configs/diffview.lua
+++ nvim_user/.config/nvim/lua/user/configs/diffview.lua
@@ -2,12 +2,21 @@ local M = {}
 
 function M.config()
   local diffview = require("user.utils").load_module("diffview")
+  -- local actions = require("user.utils").load_module("diffview.actions")
+  local actions = require("diffview.actions")
 
   diffview.setup({
     keymaps = {
-      file_history_panel = { q = "<Cmd>DiffviewClose<CR>" },
-      file_panel = { q = "<Cmd>DiffviewClose<CR>" },
-      view = { q = "<Cmd>DiffviewClose<CR>" },
+      file_history_panel = {
+        q = "<Cmd>DiffviewClose<CR>",
+      },
+      file_panel = {
+        q = "<Cmd>DiffviewClose<CR>",
+        ["d"] = actions.restore_entry,
+      },
+      view = {
+        q = "<Cmd>DiffviewClose<CR>",
+      },
     },
   })
 end
