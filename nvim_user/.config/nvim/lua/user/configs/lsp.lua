550899f9f90e331e5eac9488215a13921965d615
Aleksandar Vucic <vucinjo@gmail.com>
2022-07-18 14:33:48 +0200

    Fix multiple formatters promt

--- nvim_user/.config/nvim/lua/user/configs/lsp.lua
+++ nvim_user/.config/nvim/lua/user/configs/lsp.lua
@@ -20,6 +20,38 @@ function M.config()
     return {}
   end
 
+  local set_contains = function(set, val)
+    for key, value in pairs(set) do
+      if value == val then
+        return true
+      end
+    end
+    return false
+  end
+
+  local set_default_formatter_for_filetypes = function(language_server_name, filetypes)
+    if not set_contains(filetypes, vim.bo.filetype) then
+      return
+    end
+
+    local active_servers = {}
+
+    vim.lsp.for_each_buffer_client(0, function(client)
+      table.insert(active_servers, client.config.name)
+    end)
+
+    if not set_contains(active_servers, language_server_name) then
+      return
+    end
+
+    vim.lsp.for_each_buffer_client(0, function(client)
+      if client.name ~= language_server_name then
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+      end
+    end)
+  end
+
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
 
@@ -67,6 +99,7 @@ function M.config()
   -- end
 
   local on_attach = function(client, bufnr)
+    set_default_formatter_for_filetypes("solargraph", { "ruby" })
     -- client.resolved_capabilities.document_formatting = false
     -- client.resolved_capabilities.document_range_formatting = false
 
