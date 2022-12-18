9bdc963c102d1a6bccf55ca63b9ff6c5a19f713a
Aleksandar Vucic <vucinjo@gmail.com>
2022-12-18 11:55:01 +0100

    Fix mdeval error

--- nvim_user/.config/nvim/lua/user/configs/mdeval.lua
+++ nvim_user/.config/nvim/lua/user/configs/mdeval.lua
@@ -1,16 +1,16 @@
 local M = {}
 
 function M.setup()
-        vim.g.markdown_fenced_languages = { "ruby", "go", "javascript" }
+  vim.g.markdown_fenced_languages = { "ruby", "go", "javascript" }
 end
 
 function M.config()
-    local mdeval = require("user.core.utils").load_module("mdeval")
-    if not mdeval then
-        return {}
-    end
+  local mdeval = require("user.core.utils").load_module("mdeval")
+  if not mdeval then
+    return {}
+  end
 
-    mdeval.setup({ require_confirmation = false })
+  -- mdeval.setup({ require_confirmation = false })
 end
 
 return M
