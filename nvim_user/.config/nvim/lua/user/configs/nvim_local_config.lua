83c30e00ba615207528fa8454ebf54a81edcae81
Aleksandar Vucic <vucinjo@gmail.com>
2023-02-02 12:18:17 +0100

    Remove vim local config reloading startup message

--- nvim_user/.config/nvim/lua/user/configs/nvim_local_config.lua
+++ nvim_user/.config/nvim/lua/user/configs/nvim_local_config.lua
@@ -12,7 +12,7 @@ function M.config()
     hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
     autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
     commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
-    silent = false, -- Disable plugin messages (Config loaded/ignored)
+    silent = true, -- Disable plugin messages (Config loaded/ignored)
     lookup_parents = false, -- Lookup config files in parent directories
   })
 end
