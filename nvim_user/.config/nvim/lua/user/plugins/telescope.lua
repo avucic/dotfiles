b8a04047d1a5b4dfc88c6865b43cfef04303502e
Aleksandar Vucic <vucinjo@gmail.com>
2023-03-03 19:19:34 +0100

    replace neoclip with yanky

--- nvim_user/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim_user/.config/nvim/lua/user/plugins/telescope.lua
@@ -70,53 +70,9 @@ return {
     dependencies = { "kkharji/sqlite.lua" },
   },
   {
-    "AckslD/nvim-neoclip.lua",
-    dependencies = { "kkharji/sqlite.lua", module = "sqlite" },
-    opts = {
-      history = 1000,
-      enable_persistent_history = true,
-      length_limit = 1048576,
-      continuous_sync = true,
-      db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
-      filter = nil,
-      preview = true,
-      default_register = '"',
-      default_register_macros = "q",
-      enable_macro_history = true,
-      content_spec_column = false,
-      on_paste = {
-        set_reg = false,
-      },
-      on_replay = {
-        set_reg = false,
-      },
-      keys = {
-        telescope = {
-          i = {
-            select = "<tab>",
-            paste = "<cr>",
-            paste_behind = "<CR>",
-            replay = "<c-q>", -- replay a macro
-            delete = "<c-d>", -- delete an entry
-            custom = {},
-          },
-          n = {
-            select = "<cr>",
-            paste = "p",
-            paste_behind = "P",
-            replay = "q",
-            delete = "d",
-            custom = {},
-          },
-        },
-        fzf = {
-          select = "default",
-          paste = "ctrl-p",
-          paste_behind = "ctrl-k",
-          custom = {},
-        },
-      },
-    },
-    config = require("user.plugins.configs.neoclip"),
+    "gbprod/yanky.nvim",
+    config = function()
+      require("yanky").setup()
+    end,
   },
 }
