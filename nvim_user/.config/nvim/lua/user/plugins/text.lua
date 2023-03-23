6ee0b15d71cb6520e2e31fe0d716d7e614088aee
Aleksandar Vucic <vucinjo@gmail.com>
2023-03-23 11:54:45 +0100

    replace surround plugin

--- nvim_user/.config/nvim/lua/user/plugins/text.lua
+++ nvim_user/.config/nvim/lua/user/plugins/text.lua
@@ -1,7 +1,13 @@
 return {
   {
-    "tpope/vim-surround",
-    event = "BufRead",
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
   },
   {
     "mg979/vim-visual-multi",
