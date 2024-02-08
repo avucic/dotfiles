267a68c2fdcc152cd5e5bcf6fbec9e14b5f00f5e
Aleksandar Vucic <vucinjo@gmail.com>
2024-02-08 10:31:58 +0100

    add dial plugin

--- nvim_user/.config/nvim/lua/user/plugins/text.lua
+++ nvim_user/.config/nvim/lua/user/plugins/text.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     "mg979/vim-visual-multi",
     branch = "master",
@@ -38,5 +39,23 @@ return {
       remove_default_keybinds = true,
     },
     config = require("user.plugins.configs.nvim_toggler"),
+  },
+  {
+    "monaqa/dial.nvim",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set("n", "<C-a>", function()
+        require("dial.map").manipulate("increment", "normal")
+      end)
+      vim.keymap.set("n", "<C-x>", function()
+        require("dial.map").manipulate("decrement", "normal")
+      end)
+      vim.keymap.set("v", "<C-a>", function()
+        require("dial.map").manipulate("increment", "visual")
+      end)
+      vim.keymap.set("v", "<C-x>", function()
+        require("dial.map").manipulate("decrement", "visual")
+      end)
+    end,
   },
 }
