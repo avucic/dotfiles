8ef4464136d5af4675dbfcd6a45e979399c5170b
Aleksandar Vucic <vucinjo@gmail.com>
2024-02-08 10:27:53 +0100

    Fix neovim debugger

--- nvim_user/.config/nvim/lua/user/plugins/configs/mason_nvim_dap.lua
+++ nvim_user/.config/nvim/lua/user/plugins/configs/mason_nvim_dap.lua
@@ -1,41 +1,46 @@
-return function(_, opts)
-  -- TODO: once mason introduce ruby debugger remove this
-  local dap = require("dap")
+local Job = require("plenary.job")
+local dap = require("dap")
 
+return function(c, opts)
   require("dap.ext.vscode").load_launchjs()
 
   dap.adapters.ruby = function(callback, config)
-    local port = config.debuggerPort or os.getenv("RUBY_DEBUG_PORT") or "12345"
-    local host = config.debuggeHost or "127.0.0.1"
+    local port = config.debugPort or os.getenv("RUBY_DEBUG_PORT") or "12345"
+    local host = config.debugHost or "127.0.0.1"
+    local current_line = vim.fn.line(".")
+    local script = config.script and config.script:gsub("${currentLine}", current_line)
+    local executable = config.executable
 
-    dap.set_log_level("TRACE")
-    print("DAP: Launching debugger with", port, host, vim.inspect(config.executable))
-    callback({
-      type = "server",
-      port = port,
-      host = host,
-      enrich_config = function(config, on_config)
-        local final_config = vim.deepcopy(config)
-        local current_line = vim.fn.line(".")
-        local script = final_config.script and final_config.script:gsub("${currentLine}", current_line)
-        local executable = config.executable
+    if config.request == "launch" then
+      local command = executable and executable.command or "rdbg"
+      local args = executable and executable.args or {}
 
-        final_config.script = script
+      for i, v in pairs(executable.args) do
+        v = v:gsub("${currentLine}", current_line)
+        executable.args[i] = v
+      end
 
-        if executable then
-          for i, v in pairs(executable.args) do
-            v = v:gsub("${currentLine}", current_line)
-            executable.args[i] = v
-          end
-          final_config.executable = executable
-        end
+      Job:new({
+        command = command,
+        detached = true,
+        args = args,
+        on_stdout = function(j, return_val)
+          print("DEBUGGER stdout", return_val)
+        end,
 
-        print("DAP: Enriched config", vim.inspect(final_config))
-        on_config(final_config)
-      end,
-      command = config.command,
-      script = config.script,
-      executable = config.executable,
-    })
+        on_stderr = function(j, return_val)
+          print("DEBUGGER stderr", return_val)
+        end,
+      }):sync() -- or start()
+
+      print("RUBY DEBUGGER", vim.inspect(opts), vim.inspect(executable))
+
+      vim.defer_fn(function()
+        callback({ type = "server", host = host, port = port })
+      end, 100)
+    else
+      print("RUBY DEBUGGER", port, host)
+      callback({ type = "server", host = host, port = port })
+    end
   end
 end
