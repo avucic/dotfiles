ae06e785016cea917a6e1c6581a35dc76742a28e
Aleksandar Vucic <vucinjo@gmail.com>
2024-02-20 09:41:39 +0100

    clean

--- nvim_user/.config/nvim/after/ftplugin/ruby.lua
+++ nvim_user/.config/nvim/after/ftplugin/ruby.lua
@@ -6,20 +6,20 @@ map("c", { "comment" }, "Comment")
 local wk = require("which-key")
 
 wk.register({
-	["<leader>m"] = {
-		name = "+Rust",
-		t = {
-			name = "+Tasks",
-			t = { "<cmd>lua require('neotest').run.run()<cr>", "Run current spec" },
-			f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Run current file specs" },
-			a = { "<cmd>lua require('neotest').run.run(vim.fn.getcws())<cr>", "Run all specs" },
-			o = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Specs outline" },
-			e = { "<cmd>lua require('neotest').output.open({ enter = false })<cr>", "Specs output" },
-		},
-		d = {
-			name = "+Deubg",
-			d = { "<cmd>RustDebuggables<cr>", "Start" },
-			l = { "<cmd>RustLastDebug<cr>", "Last" },
-		},
-	},
+  ["<leader>m"] = {
+    name = "+Ruby",
+    t = {
+      name = "+Tasks",
+      t = { "<cmd>lua require('neotest').run.run()<cr>", "Run current spec" },
+      f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Run current file specs" },
+      a = { "<cmd>lua require('neotest').run.run(vim.fn.getcws())<cr>", "Run all specs" },
+      o = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Specs outline" },
+      e = { "<cmd>lua require('neotest').output.open({ enter = false })<cr>", "Specs output" },
+    },
+    d = {
+      name = "+Deubg",
+      d = { "<cmd>RustDebuggables<cr>", "Start" },
+      l = { "<cmd>RustLastDebug<cr>", "Last" },
+    },
+  },
 }, { buffer = 0, mode = "n" })
