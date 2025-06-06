9d1dbf877634d84edfc2acd7b57545607f82c892
Aleksandar Vucic <vucinjo@gmail.com>
2025-06-06 21:06:15 +0200

    refactor(nvim): Consolidate and streamline snippet configuration
    - Merge TypeScript, TypeScript React, and JavaScript snippets into javascriptreact.json.
    - Simplify LuaSnip setup by removing redundant filetype extensions and standardizing snippet path.
    - Remove unused arrow function snippets.
    - Update useState snippet prefix.

--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -1,20 +1,11 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      local vscode_snippets_path = vim.fn.expand "~/.config/nvim/snippets"
-      local snip_loader = require "luasnip.loaders.from_vscode"
-
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-      luasnip.filetype_extend("typescript", { "typescriptreact" })
-
-      snip_loader.lazy_load {
-        paths = { vscode_snippets_path },
-        override = true,
-      }
-    end,
-  },
+  "L3MON4D3/LuaSnip",
+  config = function(plugin, opts)
+    -- include the default astronvim config that calls the setup call
+    require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    -- load snippets paths
+    require("luasnip.loaders.from_vscode").lazy_load {
+      paths = { vim.fn.stdpath "config" .. "/snippets" },
+    }
+  end,
 }
